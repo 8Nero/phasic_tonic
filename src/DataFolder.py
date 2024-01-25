1ed97ffff3d22353f5aadf8d4f4504aae38db0b5
8Nero <animus030@gmail.com>
2024-01-25 09:37:28 +0100

    update

--- src/DataFolder.py
+++ src/DataFolder.py
@@ -1,16 +1,23 @@
 from pathlib import Path
 import numpy as np
 
+import logging
+logger = logging.getLogger('runtime')
+
 class DataFolder:
     def __init__(self, input_dir: str):
         """
         Initialize a DataFolder instance.
 
-        Args:
+        Parameters:
             input_dir (str): The directory containing REM recordings in .npz format.
         """
+        logger.info("Initializing a DataFolder.")
+
         self.input_dir = input_dir
         self.paths = list(Path(input_dir).rglob('*.npz'))
+
+        logger.info("DataFolder in {0} contains {1} files.".format(self.input_dir, len(self.paths)))
 
     def load(self, index: int):
         """
@@ -32,19 +39,21 @@ class DataFolder:
 
         filename = self.paths[index].stem
         name_parts = str(filename).split('_')
-        field_names = ['RatID', 'StudyDay', 'condition', 'condition_full', 'treatment', 'posstrial_number']
+        field_names = ['RatID', 'StudyDay', 'condition', 'treatment', 'posttrial_number']
         return {field: value for field, value in zip(field_names, name_parts)}
 
     def __getitem__(self, index: int):
         """
         Get both metadata and loaded data for a specific index.
 
-        Args:
+        Parameters:
             index (int): Index of the file to retrieve data from.
 
         Returns:
             dict: Dictionary containing metadata (dict) and loaded data (np.ndarray).
         """
+        logger.debug("Retrieving index number: {0}".format(index))
+
         data_dict = self.get_metadata(index)
         data = {"data" : self.load(index)}
         data["metadata"] = data_dict
