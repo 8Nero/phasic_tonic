a0a22f5ce8b46434050f2918e868fbf216f2bcc2
8Nero <tugu@mailbox.unideb.hu>
2024-05-27 16:36:16 +0200

    backup

--- src/DataFolder.py
+++ src/DataFolder.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import numpy as np
 
 import logging
-logger = logging.getLogger('runtime')
+logger = logging.getLogger('record')
 
 class DataFolder:
     def __init__(self, input_dir: str):
