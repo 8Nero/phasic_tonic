e68928c6c097c70b10855607f69c3e9a0fbe0aac
8Nero <tugu@mailbox.unideb.hu>
2024-05-27 16:25:52 +0200

    backup

--- notebooks/new_method/new_phasic5.py
+++ notebooks/new_method/new_phasic5.py
@@ -2,7 +2,6 @@ import os
 import re
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import yasa
 import psutil
 from psutil._common import bytes2human
@@ -10,11 +9,10 @@ from psutil._common import bytes2human
 from pathlib import Path
 from scipy.io import loadmat
 from mne.filter import resample
-from itertools import chain
 from tqdm import tqdm
 from sys import getsizeof
 
-from func import get_sequences, phasic_detect, get_segments, create_name_cbd, create_name_rgs, create_name_os
+from func import get_sequences, get_segments, create_name_cbd, create_name_rgs, create_name_os
 from runtime_logger import logger_setup
 
 import gc
@@ -23,10 +21,8 @@ CBD_DIR = "/home/nero/datasets/CBD/"
 RGS_DIR = "/home/nero/datasets/RGS14/"
 OS_DIR = "/home/nero/datasets/OSbasic/"
 
-OUTPUT_DIR  = "/home/nero/datasets/preprocesssed/"
-CBD_OVERVIEW_PATH = "overview.csv"
-
-overview_df = pd.read_csv(CBD_OVERVIEW_PATH)
+OUTPUT_DIR  = "/home/nero/datasets/preprocessed/"
+CBD_OVERVIEW_PATH = "/home/nero/phasic_tonic/notebooks/new_method/overview.csv"
 
 fs_cbd = 2500
 fs_os = 2500
@@ -39,110 +35,134 @@ n_down_os = fs_os/targetFs
 
 min_dur = 2
 
-def run_analysis(mapped):
-    with tqdm(mapped.keys()) as t:
-        for state in t:
-            hpc = mapped[state]
+def run(mapped, name_func, n_down):
+  with tqdm(mapped.keys()) as t:
+      for state in t:
+          hpc = mapped[state]
+  
+          title = name_func(hpc)
+          t.set_postfix_str(title) # Set the title for the progress bar
+  
+          logger.debug("Loading: {0}".format(title))
+          logger.debug("fname: {0}".format(state))
+  
+          # Load the LFP data
+          lfpHPC = loadmat(hpc)['HPC']
+          lfpHPC = lfpHPC.flatten()
+  
+          # Load the states
+          hypno = loadmat(state)['states']
+          hypno = hypno.flatten()
+  
+          # Skip if no REM epoch is detected
+          if(not (np.any(hypno == 5))):
+              logger.debug("No REM detected. Skipping.")
+              continue
 
-            title = create_name_cbd(hpc, overview_df)
-            t.set_postfix_str(title) # Set the title for the progress bar
+          logger.debug("LFP shape: {0}, size: {1}MB)".format(str(lfpHPC.shape), getsizeof(lfpHPC)//1024**2))
+          logger.debug("Memory usage: {0} ({1}%)".format(bytes2human(psutil.virtual_memory().available), str(psutil.virtual_memory().percent)))
+  
+          logger.debug("STARTED: Resampling to 500 Hz.")
+          # Downsample to 500 Hz
+          data_resample = resample(lfpHPC, down=n_down, method='fft', npad='auto')
+          logger.debug("FINISHED: Resampling to 500 Hz.")
+          logger.debug("Resampled: {0} -> {1}.".format(str(lfpHPC.shape), str(data_resample.shape)))
+          logger.debug("Memory usage: {0} ({1}%)".format(bytes2human(psutil.virtual_memory().available), str(psutil.virtual_memory().percent)))
+  
+          logger.debug("STARTED: Remove artifacts.")
+          # Remove artifacts
+          art_std, _ = yasa.art_detect(data_resample, targetFs , window=1, method='std', threshold=4, verbose='info')
+          art_up = yasa.hypno_upsample_to_data(art_std, 1, data_resample, targetFs)
+          data_resample[art_up] = 0
+          logger.debug("FINISHED: Remove artifacts.")
+  
+          data_resample = data_resample - data_resample.mean()
+          logger.debug("Resampled data shape: {0}, size: {1}MB)".format(str(data_resample.shape), getsizeof(data_resample)//1024**2))
+  
+          rem_seq = get_sequences(np.where(hypno == 5)[0])
+          # Another representation: matrix of 2 columns and n rows (n number of rem epochs), first row is the start and second is for end idx.
+  
+          # minimum duration > 2s
+          logger.debug("STARTED: Extract REM epochs.")
+          rem_seq = [(start, end) for start, end in rem_seq if (end-start) > min_dur]
+          logger.debug("REM indices: {0}.".format(rem_seq))
+  
+          # get REM segments
+          rem_idx = [(start * targetFs, (end+1) * targetFs) for start, end in rem_seq]
+          rem_epochs = get_segments(rem_idx, data_resample)
+          logger.debug("FINISHED: Extract REM epochs.")
+  
+          logger.debug("Memory usage: {0} ({1}%)".format(bytes2human(psutil.virtual_memory().available), str(psutil.virtual_memory().percent)))
+  
+          # Combine the REM indices with the corresponding downsampled segments
+          rem = {seq:seg for seq, seg in zip(rem_seq, rem_epochs)}
+  
+          fname = OUTPUT_DIR + title
+          logger.debug("Saving as: {0}.".format(title))
+          np.save(fname, rem)
+  
+          del lfpHPC, hypno, data_resample, art_std, art_up, rem_seq, rem_epochs, rem
+          gc.collect()
 
-            metadata = {}
-            metaname_list  = title.split('_')
-            metadata["rat_id"]    = int(metaname_list[0][3:])
-            metadata["study_day"] = int(metaname_list[1][2:])
-            metadata["condition"] = metaname_list[2]
-            metadata["treatment"] = int(metaname_list[3])
-            metadata["trial_num"] = int(metaname_list[4][-1])
+cbd_patterns = {
+    "posttrial":r"[\w-]+posttrial[\w-]+",
+    "hpc":"*HPC*continuous*",
+    "states":"*-states*"
+}
 
-            logger.debug("Loading: {0}".format(title))
-            logger.debug("fname: {0}".format(state))
+rgs_patterns = {
+    "posttrial":r"[\w-]+post[\w-]+trial[\w-]+",
+    "hpc":"*HPC*continuous*",
+    "states":"*-states*"
+}
 
-            # Load the LFP data
-            lfpHPC = loadmat(hpc)['HPC']
-            lfpHPC = lfpHPC.flatten()
+os_patterns = {
+    "posttrial":r".*post_trial.*",
+    "hpc":"*HPC*",
+    "states":"*states*"
+}
 
-            # Load the states
-            hypno = loadmat(state)['states']
-            hypno = hypno.flatten()
+def load_dataset(DATASET_DIR, pattern_args):
+    mapped = {}
 
-            # Skip if no REM epoch is detected
-            if(not (np.any(hypno == 5))):
-                logger.debug("No REM detected. Skipping.")
-                continue
-            elif(metadata["trial_num"] != 5):
-                logger.debug("Not Trial 5.")
-                continue
+    posttrial_pattern = pattern_args["posttrial"]
+    hpc_pattern = pattern_args["hpc"]
+    states_pattern = pattern_args["states"]
 
-            logger.debug("LFP shape: {0}, size: {1}MB)".format(str(lfpHPC.shape), getsizeof(lfpHPC)//1024**2))
-            logger.debug("Memory usage: {0} ({1}%)".format(bytes2human(psutil.virtual_memory().available), str(psutil.virtual_memory().percent)))
-
-            logger.debug("STARTED: Resampling to 500 Hz.")
-            # Downsample to 500 Hz
-            data_resample = resample(lfpHPC, down=n_down_cbd, method='fft', verbose="INFO")
-            logger.debug("FINISHED: Resampling to 500 Hz.")
-            logger.debug("Resampled: {0} -> {1}.".format(str(lfpHPC.shape), str(data_resample.shape)))
-            logger.debug("Memory usage: {0} ({1}%)".format(bytes2human(psutil.virtual_memory().available), str(psutil.virtual_memory().percent)))
-            del lfpHPC # Save memory
-
-            logger.debug("STARTED: Remove artifacts.")
-            # Remove artifacts
-            art_std, _ = yasa.art_detect(data_resample, targetFs , window=1, method='std', threshold=4, verbose='info')
-            art_up = yasa.hypno_upsample_to_data(art_std, 1, data_resample, targetFs)
-            data_resample[art_up] = 0
-            logger.debug("FINISHED: Remove artifacts.")
-
-            del art_std # Save memory
-            del art_up # Save memory
-
-            data_resample = data_resample - data_resample.mean()
-            logger.debug("Resampled data shape: {0}, size: {1}MB)".format(str(data_resample.shape), getsizeof(data_resample)//1024**2))
-
-            rem_seq = get_sequences(np.where(hypno == 5)[0])
-            # Another representation: matrix of 2 columns and n rows (n number of rem epochs), first row is the start and second is for end idx.
-
-            del hypno # Save memory
-
-            # minimum duration > 2s
-            logger.debug("STARTED: Extract REM epochs.")
-            rem_seq = [(start, end) for start, end in rem_seq if (end-start) > min_dur]
-            logger.debug("REM indices: {0}.".format(rem_seq))
-
-            # get REM segments
-            rem_idx = [(start * targetFs, (end+1) * targetFs) for start, end in rem_seq]
-            rem_epochs = get_segments(rem_idx, data_resample)
-            logger.debug("FINISHED: Extract REM epochs.")
-
-            logger.debug("Memory usage: {0} ({1}%)".format(bytes2human(psutil.virtual_memory().available), str(psutil.virtual_memory().percent)))
-            del data_resample # Save memory
-
-            # Combine the REM indices with the corresponding downsampled segments
-            rem = {seq:seg for seq, seg in zip(rem_seq, rem_epochs)}
-
-            fname = OUTPUT_DIR + title
-            logger.debug("Saving as: {0}.".format(title))
-            np.save(fname, rem)
-            
-            gc.collect() # Force garbage collection
-            
+    for root, dirs, _ in os.walk(DATASET_DIR):
+        for dir in dirs:
+            # Check if the directory is a post trial directory
+            if re.match(posttrial_pattern, dir, flags=re.IGNORECASE):
+                dir = Path(os.path.join(root, dir))
+                HPC_file = next(dir.glob(hpc_pattern))
+                states = next(dir.glob(states_pattern))
+                mapped[str(states)] = str(HPC_file)
+    return mapped
 
 if __name__ == "__main__":
     logger = logger_setup()
     logger.debug("Saving to: {0}".format(OUTPUT_DIR))
 
-    pattern = r"[\w-]+posttrial[\w-]+"
-    mapped = {}
+    mapped1 = load_dataset(CBD_DIR, pattern_args=cbd_patterns)
+    mapped2 = load_dataset(RGS_DIR, pattern_args=rgs_patterns)
+    mapped3 = load_dataset(OS_DIR, pattern_args=os_patterns)
 
-    for root, dirs, fils in os.walk(CBD_DIR):
-        for dir in dirs:
-            # Check if the directory is a post trial directory
-            if re.match(pattern, dir, flags=re.IGNORECASE):
-                dir = Path(os.path.join(root, dir))
-                HPC_file = next(dir.glob("*HPC*continuous*"))
-                states = next(dir.glob('*-states*'))
-                mapped[str(states)] = str(HPC_file)
+    print("Number of CBD recordings: ", len(mapped1))
+    print("Number of RGS14 recordings: ", len(mapped2))
+    print("Number of OS_Basic recordings: ", len(mapped3))
 
-    print("Number of recordings: ", len(mapped))
-    run_analysis(mapped)
+    # Wrapper for create name function for CBD dataset
+    def wrapper(hpc):
+        overview_df = pd.read_csv(CBD_OVERVIEW_PATH)
+        return create_name_cbd(hpc, overview_df=overview_df)
+    
+    # CBD preprocessing
+    run(mapped=mapped1, name_func=wrapper, n_down=n_down_cbd)
+
+    # RGS14 preprocessing
+    run(mapped=mapped2, name_func=create_name_rgs, n_down=n_down_rgs)
+
+    # OS basic preprocessing
+    run(mapped=mapped3, name_func=create_name_os, n_down=n_down_os)
 
     
